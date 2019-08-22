a4ac476eb80bb19aa7481bd3b41738ba50f451ac
Rohan <rohan@rmolloy.io>
2019-08-23 00:29:47 +1200

    change nill to empty str

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['ikev2_vpn']['subnet']='10.13.37.0/24'
 default['ikev2_vpn']['dns']='1.1.1.1'
-default['ikev2_vpn']['le_email']=nil
+default['ikev2_vpn']['le_email']=''
 default['ikev2_vpn']['disable_ipv6']=1
 default['ikev2_vpn']['username']='vpn'
 default['ikev2_vpn']['password']='P@ssW0rd'
