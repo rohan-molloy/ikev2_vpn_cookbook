fbff811e0737d9a5ce7ed5a495f10c877e58a461
Rohan <rohan@rmolloy.io>
2019-08-22 19:13:34 +1200

    remove redundant install code

--- recipes/config_ipsec.rb
+++ recipes/config_ipsec.rb
@@ -4,11 +4,6 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-apt_package 'Install-strongswan' do
-    package_name %w(strongswan libstrongswan-standard-plugins strongswan-libcharon libcharon-standard-plugins libcharon-extra-plugins)
-    action :install
-end
-
 template 'Generate-ipsec-config' do
     path '/etc/ipsec.conf'
     source 'ipsec.conf.erb'
